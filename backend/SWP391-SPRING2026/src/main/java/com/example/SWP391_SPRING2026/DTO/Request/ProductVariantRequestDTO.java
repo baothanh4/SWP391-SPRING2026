18216b166ad77bfbc2c0e7ec13968d09f4082758
baothanh4 <taobaothanh365@gmail.com>
2026-03-30 21:57:41 +0700

    Fix: product variant

--- backend/SWP391-SPRING2026/src/main/java/com/example/SWP391_SPRING2026/DTO/Request/ProductVariantRequestDTO.java
+++ backend/SWP391-SPRING2026/src/main/java/com/example/SWP391_SPRING2026/DTO/Request/ProductVariantRequestDTO.java
@@ -1,23 +1,13 @@
 package com.example.SWP391_SPRING2026.DTO.Request;
 
-import com.example.SWP391_SPRING2026.Enum.SaleType;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @Data
 public class ProductVariantRequestDTO {
-        private String sku;
-        private BigDecimal price;
-        private Integer stockQuantity;
-        private SaleType saleType;
-
-        private Boolean allowPreorder;
-        private Integer preorderLimit;
-        private LocalDate preorderFulfillmentDate;
-
-        private LocalDate preorderStartDate;
-        private LocalDate preorderEndDate;
-    }
+    private String sku;
+    private BigDecimal price;
+    private Integer stockQuantity;
+}
 
